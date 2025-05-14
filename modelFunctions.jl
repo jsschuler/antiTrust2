6b78f049ceeb3297d04fb2f2da8c56a150d2b330
jsschuler <jsschuler@gmail.com>
2025-05-14 09:40:33 -0400

    modified:   modelFunctions.jl

--- modelFunctions.jl
+++ modelFunctions.jl
@@ -77,7 +77,7 @@ function allSearches(tick)
     global agtCnt
     # have each agent search 50 times
     #searchCnt=rand(searchQty,agtCnt)
-    searchCnt=[50]
+    searchCnt=repeat([50],agtCnt)
     # randomize agent ordering
     searchOrder=sample(1:agtCnt,agtCnt,replace=false)
     for i in searchOrder
