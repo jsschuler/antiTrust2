02becad71f2bfa4279a8f1ebb1ee87b9db3317e8
jsschuler <jsschuler@gmail.com>
2025-06-20 12:32:36 -0400

    modified:   ctrl.jld2 	modified:   dataGen2.jl 	modified:   run.sh

--- dataGen2.jl
+++ dataGen2.jl
@@ -24,41 +24,41 @@ sweeps=5
 reps=50
 
 # generate a seed 
-seed1Vec=sort(repeat(rand(DiscreteUniform(1,10000),sweeps),reps))
-seed2Vec=rand(DiscreteUniform(1,10000),sweeps*reps)
+seed1Vec=sort(repeat(rand(DiscreteUniform(1,10000),sweeps),3*reps))
+seed2Vec=rand(DiscreteUniform(1,10000),3*sweeps*reps)
 
 # how many agents care a lot about privacy?
 # higher value means fewer care 
 #privacyValVec=sort(repeat(rand(Uniform(.1,.1),sweeps),reps))
-privacyValVec=repeat([1.0],sweeps*reps)
+privacyValVec=repeat([.1,.5,1.0],sweeps*reps)
 #privacyBeta=Beta.(1.0,privacyVal)
 # how close does the offered search result have to be before the agent accepts it?
-searchResolutionVec=repeat([.05],sweeps*reps)
+searchResolutionVec=repeat([.05,.05,.05],sweeps*reps)
 # we need a Poisson process for how many agents act exogenously 
 #switchPctVec=sort(repeat(rand(Uniform(0.05,0.05),sweeps),reps))
-switchPctVec=repeat([0.05],sweeps*reps)
+switchPctVec=repeat([0.05,.05,.05],sweeps*reps)
 #agtCntVec=sort(repeat(rand(DiscreteUniform(100,100),sweeps),reps))
-agtCntVec=repeat([100],sweeps*reps)
+agtCntVec=repeat([100,100,100],sweeps*reps)
 #poissonDist=sort(repeat(Poisson.(switchPct.*agtCnt),reps))
 # and a probability distribution for how much agents search 
 # set the Graph structure
 #pctConnectedVec=sort(repeat(rand(Uniform(),sweeps),reps))
-pctConnectedVec=repeat([.2],sweeps*reps)
+pctConnectedVec=repeat([.2,.2,.2],sweeps*reps)
 expDegreeVec=floor.(Int64,pctConnectedVec.*agtCntVec)
 #βVec=sort(repeat(rand(Uniform(),sweeps),reps))
-βVec=repeat([.5],sweeps*reps)
+βVec=repeat([.5,.5,.5],sweeps*reps)
 # Finally, we need a Poisson parameter to how much agents search
 #searchQtyVec=sort(repeat(rand(Uniform(100,100),sweeps),reps))
-searchQtyVec=repeat([100],sweeps*reps)
-modRunVec=repeat([500],sweeps*reps)
+searchQtyVec=repeat([100,100,100],sweeps*reps)
+modRunVec=repeat([500,500,500],sweeps*reps)
 
 currTime=now()
 
 ctrlFrame=DataFrame()
-ctrlFrame[!,"dateTime"]=repeat([currTime],sweeps*reps)
+ctrlFrame[!,"dateTime"]=repeat([currTime],3*sweeps*reps)
 ctrlFrame[!,"seed1"]=seed1Vec
 ctrlFrame[!,"seed2"]=seed2Vec
-ctrlFrame[!,"key"]=string.(repeat([currTime],sweeps*reps)).*"-".*string.(seed1Vec) .*"-".*string.(seed2Vec).*"-"
+ctrlFrame[!,"key"]=string.(repeat([currTime],3*sweeps*reps)).*"-".*string.(seed1Vec) .*"-".*string.(seed2Vec).*"-"
 ctrlFrame[!,"privacyVal"]=privacyValVec
 ctrlFrame[!,"searchResolution"]=searchResolutionVec
 ctrlFrame[!,"switchPct"]=switchPctVec
